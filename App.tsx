98d332ad60849d545b759e5164b4c38c165780cc
rodrigomsrocha <rodrigomtc82@gmail.com>
2022-11-20 23:50:21 -0300

    setting up eslint as linter

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello Rodrigo</Text>
+      <Text>Hello Rodrigo!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,9 +13,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#84e",
-    color: "#f0f0f0",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#84e',
+    color: '#f0f0f0',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
